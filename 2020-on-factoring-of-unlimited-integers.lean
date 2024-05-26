7fa1db140b8c346a453f81d44b674da780a5f034
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2024-05-26 09:30:12 -1000

    Update 2020-on-factoring-of-unlimited-integers.lean

--- 2020-on-factoring-of-unlimited-integers.lean
+++ 2020-on-factoring-of-unlimited-integers.lean
@@ -1,33 +1,42 @@
 import Mathlib.RingTheory.Int.Basic
+import Mathlib.Algebra.BigOperators.Basic
+/- We prove a special case of Dickson's Conjecture as stated in
+`On factoring of unlimited integers` by KAREL HRBACEK:
+ the case where each aᵢ=0.
 
-/- We prove a special case of
-Dickson's Conjecture as stated in
-`On factoring of unlimited integers`
-by KAREL HRBACEK:
-ℓ = 1, a₁ = 0.
--/
-def f {b : Nat} : Nat → Nat :=
-  λ k ↦ b * k
+Hrbacek assumes ℓ ≥ 1 but actually Dickson's Conjecture is trivial for ℓ = 0
+so we do not rule that out.
+ -/
 
-example {b:Nat} {hb : b ≥ 1} :
-  ¬ (∃ n, n > 1 ∧ ∀ k, n ∣ (@f b k))
-  →
-  ∀ n₀, ∃ n ≥ n₀, (@f b n).Prime := by
-  intro hc
-  by_cases h : b = 1
-  subst h
+open Finset
+def prod_f {ℓ : ℕ} (b : Fin ℓ → ℕ) : ℕ → ℕ :=
+  λ k ↦ Finset.prod (univ : Finset (Fin ℓ)) (λ i : Fin ℓ ↦ b i * k)
 
-  intro n₀
-  unfold f
-  simp
-  exact Nat.exists_infinite_primes n₀
-  exfalso
-  contrapose hc
-  simp
-  exists b
-  constructor
-  . exact Nat.lt_of_le_of_ne hb fun a ↦ h (id (Eq.symm a))
+example {ℓ : ℕ} {b: Fin ℓ → ℕ} {hb : ∀ i, b i ≥ 1}
+  (hc : ¬ (∃ n, n > 1 ∧ ∀ k, n ∣ (prod_f b k))) (i : Fin ℓ) (n₀:ℕ) :
+  ∃ n ≥ n₀, (b i * n).Prime := by
+  by_cases h : ∀ i, b i = 1
+  . rw [h i]
+    simp
+    exact Nat.exists_infinite_primes n₀
+  . simp at h
+    obtain ⟨i,hi⟩ := h
+    exfalso
+    contrapose hc
+    simp
+    exists b i
+    constructor
+    . exact Nat.lt_of_le_of_ne (hb i) fun a ↦ hi (id (Eq.symm a))
+    . intro k
+      let s := Finset.filter (λ x ↦ x = i)
+          (univ : (Finset (Fin ℓ)))
 
-  . intro k
-    unfold f
-    exact Nat.dvd_mul_right b k
+      have hprods: (s.prod fun x ↦ b x * k) = b i * k := by
+        show (filter (λ x ↦ x = i) univ).prod (fun x ↦ b x * k) = b i *k
+        have : (filter (λ x ↦ x = i) univ) = {i} := by
+          apply ext; intro j; simp
+        rw [this]; simp
+      let Q := prod_dvd_prod_of_subset s univ
+        (λ j : Fin ℓ ↦ b j * k) (filter_subset (fun x ↦ x = i) univ)
+      rw [hprods] at Q
+      exact Nat.dvd_trans (Nat.dvd_mul_right (b i) k) Q
